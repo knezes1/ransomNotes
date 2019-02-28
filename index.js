46d73d5f8edf89535f82a25bbbe63a1b1bda2238
knezes1 <44378619+knezes1@users.noreply.github.com>
2019-02-28 13:45:48 -0800

    Added the files lol

---
+++ index.js
@@ -0,0 +1,41 @@
+function getRandomFont() {
+  let fonts = ["Arial", "Arial Black", "Comic Sans MS", "Impact", "Lucida Sans Unicode", "Tahoma", "Trebuchet MS", "Verdana", "Georgia"];
+    a = fonts[Math.floor(Math.random() * fonts.length)];
+    console.log(a);
+  return a;
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+$(document).ready(function(){
+
+  
+  let magChrs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!?";
+  for(var i = 0; i < magChrs.length; i++){
+    $("#letters").append("<div class= 'cont ui-widget-content'>" + magChrs[i] + "</div>");
+  }
+  
+  var newLetters = [];
+
+$("#letters").children().each(function(){
+  $(this).on('mousedown', function(){
+    $(this).clone().draggable().css({'background-color' : getRandomColor(), 'color' : getRandomColor(), 'font-family' : getRandomFont()}).appendTo($("#area"));
+  });
+  
+ $("#clear").on('click', function(){
+   $("#area").empty();
+ }); 
+  
+  
+});
+});
+
+
+
